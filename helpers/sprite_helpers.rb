d08a0c99f017d34781ca3255d60576de36b11427
Reda Lemeden <reda@thoughtbot.com>
2014-07-03 20:49:51 +0200

    Refractor Swift logo

--- helpers/sprite_helpers.rb
+++ helpers/sprite_helpers.rb
@@ -1,9 +1,9 @@
 module SpriteHelpers
-  def svg_icon(icon_id, svg_class = 'svg-icon')
+  def svg_icon(icon_id, svg_class = 'svg-icon', size = 16)
     icon_id = icon_id.to_s.gsub(/_/, '-')
 
     image = image_path('sprite.svg')
-    content_tag(:svg, viewBox: '0 0 8 8', class: svg_class) do
+    content_tag(:svg, viewBox: '0 0 8 8', class: svg_class, width: size, height: size) do
       content_tag(:use, '', 'xlink:href' => "#{image}##{icon_id}", class: "#{icon_id} icon-path")
     end
   end
