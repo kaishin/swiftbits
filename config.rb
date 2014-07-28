9a97136030e8043ad2e84eedb5307c01ca621771
Reda Lemeden <reda@thoughtbot.com>
2014-07-28 11:17:50 +0000

    Disable ImageOptim

--- config.rb
+++ config.rb
@@ -32,7 +32,6 @@ configure :build do
   activate :minify_html, remove_input_attributes: false
   activate :minify_css
   activate :minify_javascript
-  activate :imageoptim
   activate :gzip, exts: %w(.js .css .html .htm .svg .ttf .otf .woff .eot)
 end
 
